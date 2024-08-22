0340e7a86e9c3024b86476623692d000f3528379
tarsibfritz <tarsilabfritz0305@outlook.com>
2024-08-22 19:41:50 -0300

    Página de registro

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,10 +1,18 @@
 const db = require('../models');
+const bcrypt = require('bcrypt');
 const User = db.User;
 
 // Criar um novo usuário
 exports.createUser = async (req, res) => {
   try {
-    const user = await User.create(req.body);
+    const { name, email, password } = req.body;
+
+    // Criptografar a senha
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Criar o usuário com a senha criptografada
+    const user = await User.create({ name, email, password: hashedPassword });
+
     res.status(201).json(user);
   } catch (error) {
     res.status(400).json({ error: error.message });
@@ -38,9 +46,22 @@ exports.getUserById = async (req, res) => {
 // Atualizar um usuário
 exports.updateUser = async (req, res) => {
   try {
-    const [updated] = await User.update(req.body, {
-      where: { id: req.params.id }
-    });
+    const { name, email, password } = req.body;
+
+    // Se houver uma nova senha, criptografá-la
+    const hashedPassword = password ? await bcrypt.hash(password, 10) : null;
+
+    const [updated] = await User.update(
+      {
+        name,
+        email,
+        ...(hashedPassword && { password: hashedPassword }), // Se houver senha nova, atualiza
+      },
+      {
+        where: { id: req.params.id }
+      }
+    );
+
     if (updated) {
       const updatedUser = await User.findByPk(req.params.id);
       res.status(200).json(updatedUser);
